4a6dfd4ad997618fad206a18d04f1a81af2073c2
Andrei <popov.andrey@outlook.com>
2023-02-13 00:21:53 +0400

    current update

---
+++ temp_files/convertor.go
@@ -0,0 +1,29 @@
+package temp_files
+
+import "fmt"
+import "github.com/zeebo/xxh3"
+
+const URL = "pw4.su/"
+
+func main() {
+	data := "https://github.com/popov-andrei"
+	hash := convertStringToHash(data)
+	fmt.Printf("XXH3 hash of '%s': %x\n", data, hash)
+
+	res := linkCompleter(hash)
+	fmt.Println("Short url:", res)
+}
+
+// Конвертор url-строки в уникальный хэш
+func convertStringToHash(s string) []byte {
+	h := xxh3.New()
+	data := []byte(s)
+	_, _ = h.Write(data)
+
+	return h.Sum(nil)
+}
+
+// Достраивать ссылки
+func linkCompleter(hash []byte) string {
+	return URL + fmt.Sprintf("%x", hash)
+}
